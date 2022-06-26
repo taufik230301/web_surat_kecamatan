61f8978d99f768f36b28dc2c22e7b4d9acf5474c
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-26 10:38:24 +0700

    add login fitur:

--- application/models/m_user.php
+++ application/models/m_user.php
@@ -15,7 +15,7 @@ class M_user extends CI_Model
     {
        $this->db->trans_start();
 
-       $this->db->query("INSERT INTO user(id_user, username, password, email ,id_user_level, id_user_detail) VALUES ('$id','$username','$password','$email','$id_user_level','$id')");
+       $this->db->query("INSERT INTO user(id_user, username, password, email ,id_user_level, id_user_detail) VALUES ('$id','$username','$pass','$email','$id_user_level','$id')");
        $this->db->query("INSERT INTO user_detail(id_user_detail, id_status_verifikasi, tanggal_registered) VALUES ('$id','$id_status_verifikasi', NOW())");
 
        $this->db->trans_complete();
--- application/views/login.php
+++ application/views/login.php
@@ -78,12 +78,12 @@
                         <form method="POST" class="register-form" id="login-form"
                             action="<?=base_url();?>Login/proses_login">
                             <div class="form-group">
-                                <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="your_name" id="your_name" placeholder="Your Name" />
+                                <label for="username"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="username" id="username" placeholder="Your Username" />
                             </div>
                             <div class="form-group">
-                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="your_pass" id="your_pass" placeholder="Password" />
+                                <label for="password"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="password" id="password" placeholder="Password" />
                             </div>
                             <div class="form-group form-button">
                                 <input type="submit" name="signin" id="signin" class="form-submit" value="Log in" />
