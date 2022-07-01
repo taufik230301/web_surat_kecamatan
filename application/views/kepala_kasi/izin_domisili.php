d1ee2412371ad128b7c4815512ca1723cd08a91b
taufik230301 <51141717+taufik230301@users.noreply.github.com>
2022-07-01 09:16:01 +0700

    add view data izin domisili  in kepala kasi

--- application/views/kepala_kasi/izin_domisili.php
+++ application/views/kepala_kasi/izin_domisili.php
@@ -53,25 +53,78 @@
                                             <th>Awal Berlaku</th>
                                             <th>Akhir Berlaku</th>
                                             <th>Nama Lengkap</th>
+                                            <th>Status Surat</th>
                                             <th>Foto Surat</th>
                                         </tr>
                                     </thead>
 
                                     <tbody>
+                                        <?php
+                                            $id = 0;
+                                            foreach($izin_domisili as $i)
+                                            :
+                                            $id++;
+                                            $id_izin_domisili = $i['id_izin_domisili'];
+                                            $id_user = $i['id_user'];
+                                            $nomor_surat = $i['nomor_surat'];
+                                            $nama_usaha = $i['nama_usaha'];
+                                            $alamat_usaha = $i['alamat_usaha'];
+                                            $kegiatan_usaha = $i['kegiatan_usaha'];
+                                            $berlaku_awal = $i['berlaku_awal'];
+                                            $berlaku_akhir = $i['berlaku_akhir'];
+                                            $nama_lengkap = $i['nama_lengkap'];
+                                            $foto_ktp = $i['foto_ktp'];
+                                            $foto_akta_usaha = $i['foto_akta_usaha'];
+                                            $foto_pengantar_lurah_setempat = $i['foto_pengantar_lurah_setempat'];
+                                            $foto_bukti_lunas_pbb = $i['foto_bukti_lunas_pbb'];
+                                            $id_status_verifikasi_surat = $i['id_status_verifikasi_surat'];
+                                            
+                                            
+                                          
+
+                                            ?>
                                         <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
-                                            <td>$320,800</td>
-                                            <td>$320,800</td>
-                                            <td>$320,800</td>
+                                            <td><?=$id?></td>
+                                            <td><?=$nomor_surat?></td>
+                                            <td><?=$nama_usaha?></td>
+                                            <td><?=$alamat_usaha?></td>
+                                            <td><?=$kegiatan_usaha?></td>
+                                            <td><?=$berlaku_awal?></td>
+                                            <td><?=$berlaku_akhir?></td>
+                                            <td><?=$nama_lengkap?></td>
+                                            <td>
+
+                                                <?php if($id_status_verifikasi_surat == 1) {?>
+                                                <div class="table-responsive">
+                                                    <div class="table table-striped table-hover ">
+                                                        <a href="" class="btn btn-warning">
+                                                            Menunggu Konfirmasi
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                                <?php }elseif($id_status_verifikasi_surat == 2) {?>
+                                                <div class="table-responsive">
+                                                    <div class="table table-striped table-hover ">
+                                                        <a href="" class="btn btn-success">
+                                                            Pengajuan Surat Di Terima
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                                <?php }elseif($id_status_verifikasi_surat == 3) {?>
+                                                <div class="table-responsive">
+                                                    <div class="table table-striped table-hover ">
+                                                        <a href="" class="btn btn-danger">
+                                                            Pengajuan Surat Ditolak
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                                <?php }?>
+                                            </td>
                                             <td>
                                                 <div class="table-responsive">
                                                     <div class="table table-striped table-hover ">
                                                         <a href="" class="btn btn-primary" data-toggle="modal"
-                                                            data-target="#foto">
+                                                            data-target="#foto<?=$id_izin_domisili?>">
                                                             Foto <i class="nav-icon fas fa-file-image"></i>
                                                         </a>
                                                     </div>
@@ -79,6 +132,72 @@
                                             </td>
 
                                         </tr>
+                                        <!-- Modal Foto-->
+                                        <div class="modal fade" id="foto<?=$id_izin_domisili?>" tabindex="-1"
+                                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog modal-xl">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel">Foto</h5>
+                                                        <button type="button" class="close" data-dismiss="modal"
+                                                            aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <div class="col">
+
+                                                            <h2>Foto KTP</h2>
+                                                            <div class="row">
+
+                                                                <a href="<?=base_url();?>assets/izin_domisili/<?=$foto_ktp?>"
+                                                                    target="_blank">
+                                                                    <img src="<?=base_url();?>assets/izin_domisili/<?=$foto_ktp?>"
+                                                                        width="100%" alt="">
+                                                                </a>
+
+                                                            </div>
+
+                                                            <br>
+                                                            <h2>Foto Pendirian Akta Usaha</h2>
+                                                            <div class="row">
+
+                                                                <a href="<?=base_url();?>assets/izin_domisili/<?=$foto_akta_usaha?>"
+                                                                    target="_blank">
+                                                                    <img src="<?=base_url();?>assets/izin_domisili/<?=$foto_akta_usaha?>"
+                                                                        width="100%" alt="">
+                                                                </a>
+                                                            </div>
+                                                            <br>
+                                                            <h2>Foto Pengantar Lurah Setempat</h2>
+                                                            <div class="row">
+
+                                                                <a href="<?=base_url();?>assets/izin_domisili/<?=$foto_pengantar_lurah_setempat?>"
+                                                                    target="_blank">
+                                                                    <img src="<?=base_url();?>assets/izin_domisili/<?=$foto_pengantar_lurah_setempat?>"
+                                                                        width="100%" alt="">
+                                                                </a>
+                                                            </div>
+                                                            <br>
+                                                            <h2>Foto Bukti Lunas PBB Tahun Bejalan</h2>
+                                                            <div class="row">
+
+                                                                <a href="<?=base_url();?>assets/izin_domisili/<?=$foto_bukti_lunas_pbb?>"
+                                                                    target="_blank">
+                                                                    <img src="<?=base_url();?>assets/izin_domisili/<?=$foto_bukti_lunas_pbb?>"
+                                                                        width="100%" alt="">
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary"
+                                                            data-dismiss="modal">Close</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <?php endforeach;?>
                                     </tbody>
                                 </table>
                             </div>
