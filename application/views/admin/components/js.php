ce6e4774a47b0d25e9675aa6cacbef0daad39d78
taufik230301 <51141717+taufik230301@users.noreply.github.com>
2022-07-06 09:51:56 +0700

    add modal log out in js for all user level

--- application/views/admin/components/js.php
+++ application/views/admin/components/js.php
@@ -25,3 +25,27 @@
 <!-- Page level custom scripts -->
 <script src="<?=base_url();?>assets/sb_admin/js/demo/datatables-demo.js"></script>
 
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Anda Yakin Untuk Logout?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Silahkan tekan tombol logout untuk keluar.
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="<?=base_url();?>Login/log_out">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
--- application/views/kepala_kasi/components/js.php
+++ application/views/kepala_kasi/components/js.php
@@ -1,16 +1,38 @@
 <!-- Bootstrap core JavaScript-->
 <script src="<?=base_url();?>assets/sb_admin/vendor/jquery/jquery.min.js"></script>
-    <script src="<?=base_url();?>assets/sb_admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?=base_url();?>assets/sb_admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="<?=base_url();?>assets/sb_admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+<!-- Core plugin JavaScript-->
+<script src="<?=base_url();?>assets/sb_admin/vendor/jquery-easing/jquery.easing.min.js"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="<?=base_url();?>assets/sb_admin/js/sb-admin-2.min.js"></script>
+<!-- Custom scripts for all pages-->
+<script src="<?=base_url();?>assets/sb_admin/js/sb-admin-2.min.js"></script>
 
-    <!-- Page level plugins -->
-    <script src="<?=base_url();?>assets/sb_admin/vendor/chart.js/Chart.min.js"></script>
+<!-- Page level plugins -->
+<script src="<?=base_url();?>assets/sb_admin/vendor/chart.js/Chart.min.js"></script>
 
-    <!-- Page level custom scripts -->
-    <script src="<?=base_url();?>assets/sb_admin/js/demo/chart-area-demo.js"></script>
-    <script src="<?=base_url();?>assets/sb_admin/js/demo/chart-pie-demo.js"></script>
+<!-- Page level custom scripts -->
+<script src="<?=base_url();?>assets/sb_admin/js/demo/chart-area-demo.js"></script>
+<script src="<?=base_url();?>assets/sb_admin/js/demo/chart-pie-demo.js"></script>
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Anda Yakin Untuk Logout?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Silahkan tekan tombol logout untuk keluar.
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-primary" href="<?=base_url();?>Login/log_out">Logout</a>
+            </div>
+        </div>
+    </div>
+</div>
